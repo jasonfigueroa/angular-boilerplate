17a10779540f9c368e007b6254a1e0e55ec7c66a
Jason Figueroa <jason.figueroa2@gmail.com>
2017-12-06 13:35:16 -0600

    successful firebase connection & post

--- app/view1/controllers/View1Ctrl.js
+++ app/view1/controllers/View1Ctrl.js
@@ -1,5 +1,50 @@
 angular
 .module("NgBoilerApp") // TODO change NgBoilerApp to app name
-.controller("View1Ctrl", function ($scope) {
-    $scope.str = "Hello from view 1";
+.controller("View1Ctrl", function ($scope, View1Factory) {
+    $scope.employee = {};
+
+    $scope.loginEmail = '';
+    $scope.loginPassword = '';
+
+    // add login event
+  $scope.login = () => {
+    const email = $scope.loginEmail;
+    const pass = $scope.loginPassword;
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  }
+
+  $scope.signUp = () => {
+    const email = $scope.loginEmail;
+    const pass = $scope.loginPassword;
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  };
+
+  $scope.logout = () => {
+    firebase.auth().signOut();
+  };
+  $scope.addEmployee = () => {
+    View1Factory.add($scope.employee);
+    // console.log($scope.employee);
+    // console.log(`first name: ${$scope.employee.fname}`);
+    // console.log(`last name: ${$scope.employee.lname}`);
+
+  };
+
+  // add a realtime listener
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser) {
+      console.log(firebaseUser);
+      console.log('logged in');
+      // btnLogout.classList.remove('hidden');
+      // adminForm.classList.remove('hidden');
+    } else {
+      console.log('not logged in');
+      // btnLogout.classList.add('hidden');
+    }
+  });
+
 })
